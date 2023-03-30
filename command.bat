42346e8523a183e580b846ee0dd3f406161782d5
shiroi36 <keita104824@gmail.com>
2023-03-30 09:08:12 +0900

    2023年3月30日杭計算テンプレート追加

--- command.bat
+++ command.bat
@@ -1,7 +1,7 @@
 git init
 git remote add origin git@github.com:shiroi36/00KeisansyoTemplate.git
 git add .
-git commit -m 2023年3月17日
+git commit -m 2023年3月30日杭計算テンプレート追加
 git push origin master
 pause
 exit
