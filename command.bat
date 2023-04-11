4cb0e7b6342f465aa00a80985b3bc83b38a95505
shiroi36 <keita104824@gmail.com>
2023-04-11 09:28:23 +0900

    2023年4月11日Sタイプの評定書を更新しました。

--- command.bat
+++ command.bat
@@ -1,7 +1,7 @@
 git init
 git remote add origin git@github.com:shiroi36/00KeisansyoTemplate.git
 git add .
-git commit -m 2023年3月30日杭計算テンプレート追加
+git commit -m 2023年4月11日Sタイプの評定書を更新しました。
 git push origin master
 pause
 exit
